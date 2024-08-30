3bfb47ef93fa4428b334aa06e6e2d0c00043e7f9
admin <gresvladimir02@gmail.com>
2024-08-30 20:33:50 +0600

    Изменения в тексте

--- app/handlers.py
+++ app/handlers.py
@@ -3,13 +3,12 @@ from aiogram.filters import CommandStart
 from aiogram.types import Message, CallbackQuery
 import app.keyboards as kb
 
-
 router = Router()
 
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.answer(f"""Добро пожаловать, {html.bold(message.from_user.first_name)}!
+    await message.answer(f"""Добро пожаловать, {message.from_user.first_name}!
 Используй кнопки ниже👇 для использования бота.
     """, reply_markup=kb.main)
 
@@ -18,6 +17,7 @@ async def cmd_start(message: Message):
 async def get_contacts(message: Message):
     await message.answer('Мои контакты 📔', reply_markup=await kb.inline_contact())
 
+
 @router.message(F.text == '✍️ Записаться на занятие')
 async def get_link_access(message: Message):
-    await message.reply(text='Запишитесь через форму! ✍️', reply_markup= await kb.link_to_form())
+    await message.reply(text='Запишитесь через форму! ✍️', reply_markup=await kb.link_to_form())
